3235de70e88f611d31c7ea98a2e25dd9fa02e6e2
unknown <imtyp0@protonmail.com>
2024-07-01 12:20:21 -0400

    good comment

--- target/debug/deps/raytracing-c1c7cace41deadde.d
+++
@@ -1,5 +0,0 @@
-C:\Users\imTyp0\Documents\Scripts\Rust\raytracing\target\debug\deps\libraytracing-c1c7cace41deadde.rmeta: src\lib.rs
-
-C:\Users\imTyp0\Documents\Scripts\Rust\raytracing\target\debug\deps\raytracing-c1c7cace41deadde.d: src\lib.rs
-
-src\lib.rs:
--- target/debug/deps/tests-05ac13724409fe6f.d
+++
@@ -1,5 +0,0 @@
-C:\Users\imTyp0\Documents\Scripts\Rust\raytracing\target\debug\deps\libtests-05ac13724409fe6f.rmeta: tests\tests.rs
-
-C:\Users\imTyp0\Documents\Scripts\Rust\raytracing\target\debug\deps\tests-05ac13724409fe6f.d: tests\tests.rs
-
-tests\tests.rs:
